d85d95b29538c9040bbb303d214a7b5380c63c9e
魔都一只土拨鼠 <imgoogege@gmail.com>
2020-06-01 01:59:37 +0800

    add

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/googege/short
 go 1.14
 
 require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/go-redis/redis/v8 v8.0.0-beta.2
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/skip2/go-qrcode v0.0.0-20200526175731-7ac0b40b2038
 	github.com/spaolacci/murmur3 v1.1.0
--- redis.go
+++ redis.go
@@ -1,1 +1,67 @@
 package short
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/golang/glog"
+	"math/rand"
+	"strconv"
+	"sync"
+)
+
+type RedisDB struct {
+	ctx    context.Context
+	client *redis.Client
+}
+
+// new redisdb
+func NewRedisDB(options *redis.Options) *RedisDB {
+	ctx := context.Background()
+	redisdb := new(RedisDB)
+	client := redis.NewClient(options)
+	redisdb.client = client
+	redisdb.ctx = ctx
+	once := new(sync.Once)
+	once.Do(func() {
+		pong, err := client.Ping(ctx).Result()
+		fmt.Println(pong, err)
+	})
+	return redisdb
+}
+
+func (rd *RedisDB) Add(v *Node) (shortURL string, err error) {
+	if _,err := rd.client.Get(rd.ctx,shortURL).Result();err !=redis.Nil {
+		v.ShortValue = v.ShortValue+strconv.FormatInt(int64(rand.Int()),10)
+	}
+	//
+	if err := rd.client.Set(rd.ctx, v.ShortValue, v.LongValue, 0).Err(); err != nil {
+		return "", err
+	} else {
+		return v.ShortValue, nil
+	}
+}
+
+func (rd *RedisDB) Delete(shortURL string) {
+	if _, err := rd.client.Del(rd.ctx, shortURL).Result(); err != nil {
+		glog.Error(err)
+	}
+}
+func (rd *RedisDB) Change(vi *Node, r string) error {
+	long, err := rd.client.Get(rd.ctx, vi.ShortValue).Result()
+	if err != nil {
+		return err
+	}
+	if err := rd.client.Set(rd.ctx, r, long, 0).Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (rd *RedisDB) Find(shortURL string) (string, error) {
+	long, err := rd.client.Get(rd.ctx, shortURL).Result()
+	if err != nil {
+		return "", err
+	}
+	return long, nil
+}
